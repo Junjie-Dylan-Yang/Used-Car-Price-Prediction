1686044e774ad9a272dbed51980f7db072e5f03b
Junjie-Dylan-Yang <dylanyang95@gmail.com>
2021-11-15 21:12:53 -0500

    Create Data Cleansing and EDA.R

---
+++ Data Cleansing and EDA.R
@@ -0,0 +1,145 @@
+library(tidyverse)
+library(lubridate)
+library(imputeTS)
+library(caret)
+library(dplyr)
+library(gutenbergr)
+library(parallel)
+library(doParallel)
+
+#--------------------------------Import Data------------------------------------
+
+data_og <- read.csv('vehicles.csv')
+
+
+
+#--------------------------Data Cleansing - Remove Outliers---------------------
+
+str(data)
+summary(data)
+
+#Save url for future verification
+data = subset(data_og,select = -c(region_url,county,lat,long, VIN, 
+                                  image_url, image_url, description, posting_date,region))
+
+#Remove entries with price=0 and some potential outliers
+summary(data$price)
+boxplot(data$price)
+
+#Price = 0
+length(which(data$price == 0))
+
+#0 < Price < 1000
+length(which(data$price > 0 & data$price <= 500))
+
+#100k and above, check data and confirm >350k are wrong data
+tenk = data[which(data$price > 100000), ]
+
+#Remain 500<price<350k
+data = data%>%filter(data$price %in% (500:350000))
+boxplot(data$price)
+
+#Year 2000 and prior
+#The scope of the PJ to consider fair trade of a used car for commute. 
+#Hence, vintage car would be excluded
+
+length(which(data$year < 2000))
+data = data%>%filter(data$year >= 2000)
+
+#Mileage, consider 50 and above.
+length(which(data$odometer < 50))
+data = data%>%filter(data$odometer >= 50)
+
+
+#--------------------------Data Cleansing - Handling NA-------------------------
+
+#All Entries are unique
+summary(duplicated(data$id))
+
+data[data == ""] <- NA
+
+colSums(is.na(data))
+
+str(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------EDA-----------------------------------------
+
+
+#Chart to show insightful distribution
+
+#Price by state
+
+#Price by model
+
+#Most common car
+
+#......
+
+
+
+
+
+#Multicollinearity 
+source("http://www.sthda.com/upload/rquery_cormat.r")
+
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!Do not run below until EDA was finished!!!!!!!!!!!!!!!!!
+
+
+#--------------------------------Data Prep-------------------------------------
+#Group Brand
+levels(data$manufacturer)
+
+luxury = c('acura','alfa-romeo','aston-martin', 'audi', 'bmw','cadillac','ferrari','harley-davidson',
+           'infiniti','jaguar','land rover','lexus','lincoln','mercedes-benz','mini','morgan',
+           'porsche','rover','tesla','volvo')
+economy = c('buick', 'chevrolet','chrysler','dodge','fiat','ford','gmc','honda','hyundai','jeep','kia',
+            'mazda','mercury','mitsubishi','nissan','pontiac','ram','saturn','subaru','toyota','volkswagen')
+
+
+data = data%>%
+  mutate(brand = ifelse(data$manufacturer %in% luxury, "luxury","common"))
+
+
+#Scale numeric variables
+
+#Change categorical variables to correct data types
+data[sapply(data, is.character)] = lapply(data[sapply(data, is.character)], as.factor)
+
+
+
+
+
+set.seed(123)
+split = createDataPartition(data$price,p = 0.7,list = FALSE)
+train = data[split,]
+
+#test set, which is considered as unseen data, 
+#is saved for the best model after models' performance comparison
+test =  data[-split,]
+
+#Validation set
+split_again = createDataPartition(train$Churn,p = 0.7,list = FALSE)
+
+train1 = train[split_again,]
+validation = train[-split_again,]
+
+
