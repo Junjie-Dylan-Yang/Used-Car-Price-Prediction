a6915277cc1a7a89df225ea27059628b348387f4
Junjie-Dylan-Yang <dylanyang95@gmail.com>
2021-11-15 22:55:12 -0500

    Update Data Cleansing and EDA.R

--- Data Cleansing and EDA.R
+++ Data Cleansing and EDA.R
@@ -20,7 +20,9 @@ summary(data)
 
 #Save url for future verification
 data = subset(data_og,select = -c(region_url,county,lat,long, VIN, 
-                                  image_url, image_url, description, posting_date,region))
+                                  image_url, image_url, description,region))
+
+data$posting_date <- round_date(as.Date(data$posting_date),"day")
 
 #Remove entries with price=0 and some potential outliers
 summary(data$price)
@@ -30,13 +32,13 @@ boxplot(data$price)
 length(which(data$price == 0))
 
 #0 < Price < 1000
-length(which(data$price > 0 & data$price <= 500))
+length(which(data$price > 0 & data$price <= 1000))
 
 #100k and above, check data and confirm >350k are wrong data
 tenk = data[which(data$price > 100000), ]
 
 #Remain 500<price<350k
-data = data%>%filter(data$price %in% (500:350000))
+data = data%>%filter(data$price %in% (1000:350000))
 boxplot(data$price)
 
 #Year 2000 and prior
@@ -48,7 +50,10 @@ data = data%>%filter(data$year >= 2000)
 
 #Mileage, consider 50 and above.
 length(which(data$odometer < 50))
-data = data%>%filter(data$odometer >= 50)
+length(which(data$odometer > 300000))
+data = data%>%filter(data$odometer %in% (50:300000))
+
+#Mileage, consider 500k and below.
 
 
 #--------------------------Data Cleansing - Handling NA-------------------------
@@ -62,25 +67,32 @@ colSums(is.na(data))
 
 str(data)
 
+#If del rows with any NA
+data_removena = data[complete.cases(data), ]
 
 
 
+#--------------------------------feature engineering---------------------------
+
+#Group Brand
+levels(data$manufacturer)
+
+luxury = c('acura','alfa-romeo','aston-martin', 'audi', 'bmw','cadillac','ferrari','harley-davidson',
+           'infiniti','jaguar','land rover','lexus','lincoln','mercedes-benz','mini','morgan',
+           'porsche','rover','tesla','volvo')
+economy = c('buick', 'chevrolet','chrysler','dodge','fiat','ford','gmc','honda','hyundai','jeep','kia',
+            'mazda','mercury','mitsubishi','nissan','pontiac','ram','saturn','subaru','toyota','volkswagen')
 
 
-
-
-
-
-
-
-
+data_removena = data_removena%>%
+  mutate(brand = ifelse(data_removena$manufacturer %in% luxury, "luxury","common"))
 
 
 
 
 
 #-----------------------------------EDA-----------------------------------------
-
+#Do EDA on data_removena
 
 #Chart to show insightful distribution
 
@@ -89,6 +101,10 @@ str(data)
 #Price by model
 
 #Most common car
+
+#Which day has the most posting? Which days tend to have more expensive posting?
+
+#common vs luxury distribution?
 
 #......
 
@@ -101,31 +117,38 @@ source("http://www.sthda.com/upload/rquery_cormat.r")
 
 
 
-#!!!!!!!!!!!!!!!!!!!!!!!Do not run below until EDA was finished!!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!!!!!!!!!!!!!!!!!!!!!!!Save for Later!!!!!!!!!!!!!!!!!
 
 
 #--------------------------------Data Prep-------------------------------------
-#Group Brand
-levels(data$manufacturer)
 
-luxury = c('acura','alfa-romeo','aston-martin', 'audi', 'bmw','cadillac','ferrari','harley-davidson',
-           'infiniti','jaguar','land rover','lexus','lincoln','mercedes-benz','mini','morgan',
-           'porsche','rover','tesla','volvo')
-economy = c('buick', 'chevrolet','chrysler','dodge','fiat','ford','gmc','honda','hyundai','jeep','kia',
-            'mazda','mercury','mitsubishi','nissan','pontiac','ram','saturn','subaru','toyota','volkswagen')
-
-
-data = data%>%
-  mutate(brand = ifelse(data$manufacturer %in% luxury, "luxury","common"))
 
 
 #Scale numeric variables
 
 #Change categorical variables to correct data types
 data[sapply(data, is.character)] = lapply(data[sapply(data, is.character)], as.factor)
-
-
-
 
 
 set.seed(123)
